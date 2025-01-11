d8cd43e27579d1387788d963c380d1db25e4807c
unknown <preetambb812@gmail.com>
2025-01-11 21:35:48 +0530

    first commit

--- print.java
+++ print.java
@@ -3,7 +3,7 @@ public class print {
     public static void main(String[] args) {
         
         int n=4;
-        for(int i=1; i<=4; i++){
+        for(int i=n; i<=1; i--){
             for(int j=1; j<=i; j++){
                 System.out.print("*");
             }
